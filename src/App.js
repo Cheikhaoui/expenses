c1ba1f35757dd89c14a64d8d1e9bcb93a811e538
hcheikhaoui <h.cheikhaoui@rmaassurance.com>
2021-10-27 16:40:57 +0100

    add expenses components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
+  const [expenses,setExpenses] = useState([{
+    title : 'title 1',
+    date : new Date(12,16,1993),
+    amount : 1725
+  }]);
+
+  const onAddExpense = (expense)=>{
+    expenses.push(expense);
+    setExpenses([...expenses]);
+   
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense = {onAddExpense}/>
+      <Expenses items = {expenses}></Expenses>
     </div>
   );
 }
